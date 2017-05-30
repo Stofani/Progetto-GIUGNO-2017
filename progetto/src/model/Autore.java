93d6cd50bfbd2bc384e37e9969478f215a768b5e
Vincenzo Martello <vmartello.95@gmail.com>
2017-05-30 12:22:17 +0200

    aggiunte classi del model

---
+++ progetto/src/model/Autore.java
@@ -0,0 +1,63 @@
+package model;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+public class Autore {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String nome;
+	private String cognome;
+	private String nazionalità;
+	@Temporal(TemporalType.DATE)
+	private LocalDate dataDiNascita;
+	@Temporal(TemporalType.DATE)
+	private LocalDate dataDiMorte;
+	private List<Quadro> quadri;
+	public Autore(){
+		quadri=new LinkedList<Quadro>();
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getNazionalità() {
+		return nazionalità;
+	}
+	public void setNazionalità(String nazionalità) {
+		this.nazionalità = nazionalità;
+	}
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+	public LocalDate getDataDiMorte() {
+		return dataDiMorte;
+	}
+	public void setDataDiMorte(LocalDate dataDiMorte) {
+		this.dataDiMorte = dataDiMorte;
+	}
+	public List<Quadro> getQuadri() {
+		return quadri;
+	}
+	public void addQuadro(Quadro quadro){
+		this.quadri.add(quadro);
+	}
+}
